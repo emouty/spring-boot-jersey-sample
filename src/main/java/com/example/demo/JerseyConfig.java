d919cc11a71df7cbf11ec998ae6869644db309fb
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2025-02-13 01:48:00 +0200

    init

--- src/main/java/com/example/demo/JerseyConfig.java
+++ src/main/java/com/example/demo/JerseyConfig.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import io.micrometer.observation.ObservationRegistry;
-import org.glassfish.jersey.micrometer.server.ObservationRequestEventListener;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
