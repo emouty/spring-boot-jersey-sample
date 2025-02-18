621cbc64fc8abe76e0a24979718ac49973374cb3
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2025-02-18 13:08:07 +0200

    print env

--- webapp/src/main/java/com/example/demo/DemoApplication.java
+++ webapp/src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,23 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static java.util.stream.Collectors.joining;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		var env = System.getenv().entrySet()
+				.stream()
+				.map(entry -> entry.getKey() + "=" + entry.getValue())
+				.collect(joining(",\n"));
+		System.out.println("**********************************");
+		System.out.println("**********************************");
+		System.out.println("**********  ENV  *****************");
+		System.out.println(env);
+
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
